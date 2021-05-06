f6b36e542e1f9b81245f958bc3878ed642861aff
Evangelos Karachristos <karachristos.e@outlook.com>
2021-05-06 16:05:42 +0300

    hide

--- my CV/FPGAsRe.Designer.cs
+++ my CV/FPGAsRe.Designer.cs
@@ -62,6 +62,7 @@ namespace my_CV
             this.button1.Text = "Read Research";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FPGAsRe
             // 
--- my CV/FPGAsRe.cs
+++ my CV/FPGAsRe.cs
@@ -21,5 +21,10 @@ namespace my_CV
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
     }
 }
