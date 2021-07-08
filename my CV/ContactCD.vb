8ac13e530bd2e2aa8be9c4a3548262a47a70bf3c
Evangelos Karachristos <karachristos.e@outlook.com>
2021-07-08 20:53:55 +0300

    1

---
+++ my CV/ContactCD.vb
@@ -0,0 +1,8 @@
+﻿
+Private Sub button2_Click(ByVal sender As Object, ByVal e As EventArgs)
+    Dim uri = "https://www.linkedin.com/in/karachristos-e/"
+    Dim psi = New ProcessStartInfo()
+    psi.UseShellExecute = True
+    psi.FileName = uri
+    Process.Start(psi)
+End Sub
